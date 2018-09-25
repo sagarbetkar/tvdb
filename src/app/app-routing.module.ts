b90defbfc7e11529e89e4f818a5df5bff371e39a
Sagar Betkar <sagarbetkar1994@gmail.com>
2018-09-25 08:34:27 +0530

    Added tvdb angular 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [{
+  path: 'signup',
+  component: SignupComponent,
+},{
+  path: 'login',
+  component: LoginComponent,
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
